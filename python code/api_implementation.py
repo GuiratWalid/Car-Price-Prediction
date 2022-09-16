9e567c96d01ea41a634ad43630e7137fb78fba00
unknown <guiratwalid98@gmail.com>
2022-09-16 15:39:09 +0100

    create apis and deployment

---
+++ python code/api_implementation.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 15:25:18 2022
+
+@author: Walid
+"""
+
+import json
+import requests
+
+
+url1 = 'http://localhost:8000/Car_Price_Linear_Regression'
+
+url2 = 'http://localhost:8000/Car_Price_Lasso_Regression'
+
+input_data_for_model = {
+    
+    'Year': 2011,
+    'Present_Price': 4.15,
+    'Kms_Driven': 5200,
+    'Fuel_Type': 0,
+    'Seller_Type': 0,
+    'Transmission': 0,
+    'Owner': 0,
+
+    }
+
+# Linear Regression Model
+input_json = json.dumps(input_data_for_model)
+
+response = requests.post(url1, data= input_json)
+
+print("Linear Regression: ",response.text)
+
+
+# Lasso Regression Model
+input_json = json.dumps(input_data_for_model)
+
+response = requests.post(url2, data= input_json)
+
+print("Lasso Regression: ",response.text)
---
+++ python code/ml_apis.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 14:09:38 2022
+
+@author: Walid
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import json
+
+
+app = FastAPI()
+
+
+class model_input(BaseModel):
+    Year: int
+    Present_Price: float
+    Kms_Driven: int
+    Fuel_Type: int
+    Seller_Type: int
+    Transmission: int
+    Owner: int
+
+
+# Loading the saved models
+linear_regression_model = pickle.load(open('Car_Price_Model_Linear_Regression.sav','rb'))
+lasso_regression_model = pickle.load(open('Car_Price_Model_Lasso_Regression.sav','rb'))
+
+# Input Traitment
+def get_Input(input_parameters: model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    year = input_dictionary['Year']
+    pre = input_dictionary['Present_Price']
+    kms = input_dictionary['Kms_Driven']
+    fuel = input_dictionary['Fuel_Type']
+    sel = input_dictionary['Seller_Type']
+    trans = input_dictionary['Transmission']
+    own = input_dictionary['Owner']
+    
+    input_list = [year, pre, kms, fuel, sel, trans, own]
+    
+    return input_list
+
+
+@app.post('/Car_Price_Linear_Regression')
+def diabetes_pred(input_parameters: model_input):
+    
+    input_list = get_Input(input_parameters)
+    
+    prediction = linear_regression_model.predict([input_list])
+    
+    return prediction[0]
+    
+    
+@app.post('/Car_Price_Lasso_Regression')
+def diabetes_pred(input_parameters: model_input):
+    
+    input_list = get_Input(input_parameters)
+    
+    prediction = lasso_regression_model.predict([input_list])
+    
+    return prediction[0]
